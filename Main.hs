8b23748143d393c3a28a161a167a861ca3f73587
Daniel Choi <dhchoi@gmail.com>
2016-05-24 13:50:37 -0400

    can configure null output string

--- Main.hs
+++ Main.hs
@@ -10,6 +10,7 @@ import Data.List (intercalate)
 data Options = Options {
       parseHtml :: Bool
     , outputDelimiter :: String
+    , nullOutput :: String
     , xpathBase :: String
     , childXPaths :: [String]
     } deriving Show
@@ -17,8 +18,8 @@ data Options = Options {
 options :: Parser Options
 options = Options 
     <$> flag yes no (long "xml" <> help "Parse as XML, rather than HTML.")
-    <*> strOption (short 'F' <> metavar "OUTPUT-DELIM" <> help "Default \\t"
-                   <> value "\t")
+    <*> strOption (short 'F' <> metavar "OUTPUT-DELIM" <> help "Default \\t" <> value "\t")
+    <*> strOption (short 'n' <> metavar "NULL-OUTPUT" <> help "Null value output string. Default \"\"" <> value "")
     <*> strArgument (metavar "BASE-XPATH")
     <*> many (strArgument (metavar "CHILD-XPATH"))
 
@@ -30,7 +31,7 @@ opts = info (helper <*> options)
 main :: IO ()
 main = do
   o@Options{..} <- execParser opts
-  let xpaths'' = mkXPaths childXPaths
+  let xpaths'' = mkXPaths nullOutput childXPaths
   r <- runX (readDocument [withValidate no, withWarnings no
                 , withParseHTML parseHtml, withInputEncoding utf8] ""
             >>> extractDSV xpathBase xpaths''
@@ -38,14 +39,14 @@ main = do
   mapM_ (putStrLn . intercalate outputDelimiter) $ concat r
 
 
-mkXPaths :: ArrowXml a => [String] -> a XmlTree [String]
-mkXPaths [] = mkXPaths ["."]
-mkXPaths xpaths =
+mkXPaths :: ArrowXml a => String -> [String] -> a XmlTree [String]
+mkXPaths n [] = mkXPaths n ["."]
+mkXPaths nullOutput' xpaths =
     let arrows :: ArrowXml a => [a XmlTree String]
         arrows = map (\x -> (listA
-                              (getXPathTrees x >>> getText' >>> cleanText )
-                              >>^ concat)
-                            `orElse` constA "")
+                              (getXPathTrees x >>> getText' >>> cleanText)
+                              >>^ concat >>> replaceNull nullOutput')
+                            `orElse` constA nullOutput')
                       xpaths
     in listA (catA arrows)
 
@@ -54,6 +55,8 @@ getText' = (isText >>> getText) `orElse` xshow this
 
 cleanText :: ArrowXml a => a String String
 cleanText = arr (T.unpack . T.concatMap escapeNewLines . T.strip . T.pack)
+
+replaceNull nullOutput' = arr (\x -> if null x then nullOutput' else x)
 
 escapeNewLines '\n' = " "
 escapeNewLines '\r' = " "
